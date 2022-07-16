ba8a983642ca21ab84a9a0ece664b2d9684fa189
Gautham Vijayan <kinggautham495@gmail.com>
2022-07-16 14:12:34 +0530

    Create firebaseconfig.js

---
+++ public/firebaseconfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "__",
+  authDomain: "__",
+  projectId: "__",
+  storageBucket: "__",
+  messagingSenderId:"__",
+  appId: "__",
+  measurementId: "__",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
